7196209338b46782a6bf8048be70bf9a7e1bed51
Jonathan Fok kan <jfokkan@inocybe.ca>
2014-12-03 19:36:38 -0500

    ADDED links for about and records page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick.DBAction
 import models.SSMUProfiles
 import models.NewsItems
+import models.Users
 
 object Application extends Controller {
 
@@ -30,31 +31,45 @@ object Application extends Controller {
 	def about = DBAction { implicit request =>
 	    implicit val session = request.dbSession
 	    val team = SSMUProfiles.profiles.list
-	    println(team)
+	    val teamUsers = team.map{ profile =>
+	        (for{u <- Users.users if u.id === profile.userId } yield u).list.head
+	    }
 	    
 	    val playSession = request.session
 		playSession.get("connected").map { user =>
 		    val username = playSession.get("username").get
-			Ok(views.html.about(team, loggedIn=true, username=username)).withSession(playSession)
+			Ok(views.html.about(team, teamUsers, loggedIn=true, username=username)).withSession(playSession)
 		}.getOrElse {
 			//Unauthorized("Oops, you are not connected")
-			Ok(views.html.about(team))
+			Ok(views.html.about(team, teamUsers))
 		}
 	}
 
 	def records = DBAction { implicit request =>
 	    implicit val session = request.dbSession
-	    val menMeet = (for{record <- SSMURecords.records if record.sex === "male" && record.competition} yield record)
-	    val womenMeet = (for{record <- SSMURecords.records if record.sex === "female" && record.competition} yield record)
-	    val menTraining = (for{record <- SSMURecords.records if record.sex === "male" && !record.competition} yield record)
-	    val womenTraining = (for{record <- SSMURecords.records if record.sex === "female" && !record.competition} yield record)
+	    val menMeet = (for{record <- SSMURecords.records if record.sex === "male" && record.competition} yield record).list
+	    val womenMeet = (for{record <- SSMURecords.records if record.sex === "female" && record.competition} yield record).list
+	    val menTraining = (for{record <- SSMURecords.records if record.sex === "male" && !record.competition} yield record).list
+	    val womenTraining = (for{record <- SSMURecords.records if record.sex === "female" && !record.competition} yield record).list
+	    val mmrUsers = menMeet.map{ record =>
+	        (for{u <- Users.users if u.id === record.userId } yield u).list.head
+	    }
+	    val wmrUsers = womenMeet.map{ record =>
+	        (for{u <- Users.users if u.id === record.userId } yield u).list.head
+	    }
+	    val mtrUsers = menTraining.map{ record =>
+	        (for{u <- Users.users if u.id === record.userId } yield u).list.head
+	    }
+	    val wtrUsers = womenTraining.map{ record =>
+	        (for{u <- Users.users if u.id === record.userId } yield u).list.head
+	    }
 
 	    val playSession = request.session
 		playSession.get("connected").map { user =>
 		    val username = playSession.get("username").get
-			Ok(views.html.records(menMeet.list, womenMeet.list, menTraining.list, womenTraining.list, loggedIn=true, username=username)).withSession(playSession)
+			Ok(views.html.records(menMeet, womenMeet, menTraining, womenTraining,mmrUsers,wmrUsers,mtrUsers,wtrUsers,loggedIn=true, username=username)).withSession(playSession)
 		}.getOrElse {
-			Ok(views.html.records(menMeet.list, womenMeet.list, menTraining.list, womenTraining.list))
+			Ok(views.html.records(menMeet, womenMeet, menTraining, womenTraining,mmrUsers,wmrUsers,mtrUsers,wtrUsers))
 		}
 	}
 	
